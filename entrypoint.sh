82696e6d5a39df9c94b0ac0f8361866f14d5ab91
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2022-05-24 09:45:33 +0200

    More testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,6 +44,9 @@ echo "[+] Set directory is safe ($CLONE_DIR)"
 git config --global --add safe.directory "$CLONE_DIR"
 # git config --global --add safe.directory /github/workspace
 
+echo "[+] Pull Git LFS objects"
+# Pull lfs objects from source repo
+git-lfs pull
 
 echo "[+] Checking if remote exists"
 if [[ -z "$(git ls-remote "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git")" ]]; then
@@ -59,7 +62,6 @@ fi
 if [ "$(git ls-remote --heads "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" $TARGET_BRANCH | wc -l)" == "1" ]; then
 	echo "[+] Target branch exists, cloning repo"
 	git clone --single-branch --branch "$TARGET_BRANCH" "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
-	# git lfs pull
 else
 	echo "[-] Target branch does not exist"
 
@@ -76,75 +78,76 @@ else
 	fi
 fi
 
-echo "[+] We are cloned and ready to go!"
-git status
-git remote -v
-
-cd "$CLONE_DIR"
-git status
-git remote -v
-
-# # ls -la "$CLONE_DIR"
-
-# TEMP_DIR=$(mktemp -d)
-# # This mv has been the easier way to be able to remove files that were there
-# # but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
-# # including "." and with the exception of ".git/"
-# mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
-
-# # $TARGET_DIRECTORY is '' by default
-# ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
-
-# echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
-# rm -rf "$ABSOLUTE_TARGET_DIRECTORY"
-
-# echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
-# mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
-
-# echo "[+] Listing Current Directory Location"
-# ls -al
-
-# echo "[+] Listing root Location"
-# ls -al /
-
-# mv "$TEMP_DIR/.git" "$CLONE_DIR/.git"
-
-# echo "[+] List contents of $SOURCE_DIRECTORY"
-# ls "$SOURCE_DIRECTORY"
-
-# echo "[+] Checking if local $SOURCE_DIRECTORY exist"
-# if [ ! -d "$SOURCE_DIRECTORY" ]
-# then
-# 	echo "ERROR: $SOURCE_DIRECTORY does not exist"
-# 	echo "This directory needs to exist when push-to-another-repository is executed"
-# 	echo
-# 	echo "In the example it is created by ./build.sh: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L19"
-# 	echo
-# 	echo "If you want to copy a directory that exist in the source repository"
-# 	echo "to the target repository: you need to clone the source repository"
-# 	echo "in a previous step in the same build section. For example using"
-# 	echo "actions/checkout@v3. See: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L16"
-# 	exit 1
-# fi
-
-# echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
-# cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
-# cd "$CLONE_DIR"
-
-# echo "[+] Files that will be pushed"
-# ls -la
-
-# git checkout "$TARGET_BRANCH"
-
-# ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-# COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
-# COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
-
-# echo "[+] Adding git commit"
-# git add .
-
-# echo "[+] git status:"
+# echo "[+] We are cloned and ready to go!"
 # git status
+# git remote -v
+
+# cd "$CLONE_DIR"
+# git status
+# git remote -v
+
+# ls -la "$CLONE_DIR"
+
+TEMP_DIR=$(mktemp -d)
+# This mv has been the easier way to be able to remove files that were there
+# but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
+# including "." and with the exception of ".git/"
+mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
+
+# $TARGET_DIRECTORY is '' by default
+ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
+
+echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
+rm -rf "$ABSOLUTE_TARGET_DIRECTORY"
+
+echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
+mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
+
+echo "[+] Listing Current Directory Location"
+ls -al
+
+echo "[+] Listing root Location"
+ls -al /
+
+mv "$TEMP_DIR/.git" "$CLONE_DIR/.git"
+
+echo "[+] List contents of $SOURCE_DIRECTORY"
+ls "$SOURCE_DIRECTORY"
+
+echo "[+] Checking if local $SOURCE_DIRECTORY exist"
+if [ ! -d "$SOURCE_DIRECTORY" ]
+then
+	echo "ERROR: $SOURCE_DIRECTORY does not exist"
+	echo "This directory needs to exist when push-to-another-repository is executed"
+	echo
+	echo "In the example it is created by ./build.sh: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L19"
+	echo
+	echo "If you want to copy a directory that exist in the source repository"
+	echo "to the target repository: you need to clone the source repository"
+	echo "in a previous step in the same build section. For example using"
+	echo "actions/checkout@v3. See: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L16"
+	exit 1
+fi
+
+echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
+cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
+cd "$CLONE_DIR"
+
+echo "[+] Files that will be pushed"
+ls -la
+
+# TODO: We are either already
+git checkout -b "$TARGET_BRANCH"
+
+ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
+
+echo "[+] Adding git commit"
+git add .
+
+echo "[+] git status:"
+git status
 
 # echo "[+] showing git refs:"
 # git show-ref
@@ -152,16 +155,16 @@ git remote -v
 # # echo "[+] git diff-index:"
 # # # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 # # git diff-index --quiet HEAD || 
-# echo "[+] comitting changes"
-# git commit --message "$COMMIT_MESSAGE"
+echo "[+] comitting changes"
+git commit --message "$COMMIT_MESSAGE"
 
-# echo "[+] listing remotes"
-# git remote -v
+echo "[+] listing remotes"
+git remote -v
 
-# echo "[+] Pushing git commit"
-# # # # --set-upstream: sets the branch when pushing to a branch that does not exist
-# # git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --set-upstream "$TARGET_BRANCH"
-# git push origin "$TARGET_BRANCH" --set-upstream
+echo "[+] Pushing git commit"
+# # # --set-upstream: sets the branch when pushing to a branch that does not exist
+# git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --set-upstream "$TARGET_BRANCH"
+git push origin "$TARGET_BRANCH" --set-upstream
 
-# echo "[+] Pushing LFS files"
-# git-lfs push
+echo "[+] Pushing LFS files"
+git-lfs push

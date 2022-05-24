fc7a2dbd5b22cb42f96f5a3d9866120f72aa97ad
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2022-05-24 10:21:59 +0200

    4

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,9 +47,10 @@ git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
 
 echo "[+] Checking out LFS objects"
+git config --global --add safe.directory /github/workspace/
+# git config --global --add safe.directory /github/workspace/"$SOURCE_DIRECTORY"
 pwd
 cd "$SOURCE_DIRECTORY"
-git config --global --add safe.directory /github/workspace/"$SOURCE_DIRECTORY"
 pwd
 git status
 cd /github/workspace/
